32f54bac4faef32665a09d523087974c2e4501c8
Dimitrije <dimitrije24000@gmail.com>
2022-05-28 21:39:13 +0200

    feat: initial project structure

---
+++ isa/src/main/java/com/ftnisa/isa/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.ftnisa.isa.controller;
+
+import com.ftnisa.isa.model.user.User;
+import com.ftnisa.isa.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity(userService.getAllUsers(), HttpStatus.OK);
+    }
+}
---
+++ isa/src/main/java/com/ftnisa/isa/service/UserService.java
@@ -0,0 +1,9 @@
+package com.ftnisa.isa.service;
+
+import com.ftnisa.isa.model.user.User;
+
+import java.util.List;
+
+public interface UserService {
+    public List<User> getAllUsers();
+}
