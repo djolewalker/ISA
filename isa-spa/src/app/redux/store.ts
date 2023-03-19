c148a4512b509d5471cab64a06be25c6677233ad
Dimitrije <dimitrije24000@gmail.com>
2023-03-19 13:45:27 +0100

    feat: initial react app structure

---
+++ isa-spa/src/app/redux/store.ts
@@ -0,0 +1,19 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+
+import rootSaga from 'app/redux/saga';
+
+const reducer = combineReducers({});
+
+const sagaMiddleware = createSagaMiddleware();
+
+export const store = configureStore({
+  reducer,
+  middleware: [sagaMiddleware],
+  devTools: true
+});
+
+sagaMiddleware.run(rootSaga);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
