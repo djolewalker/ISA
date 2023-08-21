4d49cf53492399e05724c14023f01437b3656df2
Dimitrije <dimitrije24000@gmail.com>
2023-08-21 19:06:28 +0200

    fix: resolve merge conflicts

--- isa/src/main/java/com/ftnisa/isa/dto/ride/RideDto.java
+++ isa/src/main/java/com/ftnisa/isa/dto/ride/RideDto.java
@@ -1,24 +1,13 @@
 package com.ftnisa.isa.dto.ride;
 
-import com.ftnisa.isa.dto.location.LocationDto;
 import com.ftnisa.isa.dto.route.RouteDto;
-import com.ftnisa.isa.dto.user.DriverDto;
-import com.ftnisa.isa.dto.vehicle.VehicleTypeDto;
-import com.ftnisa.isa.model.ride.Rejection;
-import com.ftnisa.isa.model.ride.RideStatus;
-import com.ftnisa.isa.model.ride.RouteOptimizationCriteria;
-import com.ftnisa.isa.model.route.Route;
-import com.ftnisa.isa.model.user.Driver;
-import com.ftnisa.isa.model.vehicle.VehicleType;
-
+import com.ftnisa.isa.dto.user.DriverResponse;
+import com.ftnisa.isa.dto.vehicle.VehicleTypeResponse;
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 public class RideDto {
-
-
     private LocalDateTime startTime;
 
     private LocalDateTime finishTime;
@@ -39,33 +28,16 @@ public class RideDto {
 
     private Boolean babyTransportFlag;
 
-    private DriverDto driver;
+    private DriverResponse driver;
 
     private List<RouteDto> routes;
 
     private RejectionDto rejection;
 
-    private VehicleTypeDto vehicleType;
+    private VehicleTypeResponse vehicleType;
 
 
     public RideDto() {
-    }
-
-    public RideDto(LocalDateTime startTime, LocalDateTime finishTime, int numberOfPassengers, float totalPrice, Duration estimatedDuration, RideStatusDto rideStatus, RouteOptimizationCriteriaDto routeOptimizationCriteria, Boolean panicFlag, Boolean petTransportFlag, Boolean babyTransportFlag, DriverDto driver, List<RouteDto> routes, RejectionDto rejection, VehicleTypeDto vehicleType) {
-        this.startTime = startTime;
-        this.finishTime = finishTime;
-        this.numberOfPassengers = numberOfPassengers;
-        this.totalPrice = totalPrice;
-        this.estimatedDuration = estimatedDuration;
-        this.rideStatus = rideStatus;
-        this.routeOptimizationCriteria = routeOptimizationCriteria;
-        this.panicFlag = panicFlag;
-        this.petTransportFlag = petTransportFlag;
-        this.babyTransportFlag = babyTransportFlag;
-        this.driver = driver;
-        this.routes = routes;
-        this.rejection = rejection;
-        this.vehicleType = vehicleType;
     }
 
     public LocalDateTime getStartTime() {
@@ -148,14 +120,6 @@ public class RideDto {
         this.babyTransportFlag = babyTransportFlag;
     }
 
-    public DriverDto getDriver() {
-        return driver;
-    }
-
-    public void setDriver(DriverDto driver) {
-        this.driver = driver;
-    }
-
     public List<RouteDto> getRoutes() {
         return routes;
     }
@@ -170,13 +134,5 @@ public class RideDto {
 
     public void setRejection(RejectionDto rejection) {
         this.rejection = rejection;
-    }
-
-    public VehicleTypeDto getVehicleType() {
-        return vehicleType;
-    }
-
-    public void setVehicleType(VehicleTypeDto vehicleType) {
-        this.vehicleType = vehicleType;
     }
 }
