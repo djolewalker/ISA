6c7c64ad538a793ac16ce2fa963b086d1d6b478c
David <ns.erosdavid@gmail.com>
2023-09-13 17:00:03 +0200

    test: set up test class for ride service

---
+++ isa/src/test/java/com/ftnisa/isa/RideServiceTests.java
@@ -0,0 +1,46 @@
+package com.ftnisa.isa;
+
+
+import com.ftnisa.isa.service.RideService;
+import com.ftnisa.isa.service.RideServiceImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+
+
+
+public class RideServiceTests {
+
+
+    @BeforeEach
+    public void setUp(){
+
+    }
+
+    @AfterEach
+    public void tearDown(){
+
+    }
+
+
+    @Test
+    public void testQuickRideBookingOnePassenger(){
+        assertThat(1).isGreaterThan(0) ;
+
+
+    }
+
+}
